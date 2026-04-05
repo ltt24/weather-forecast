12be72b17228a849c7703b26ed29a1933bc0f7ab
ltt24 <dovohuongthao4902@gmail.com>
2026-04-05 22:23:50 +0700

    Today's weather

---
+++ exercise1.js
@@ -0,0 +1,12 @@
+let temperature = 75;
+let conditions = "sunny";
+let sunriseTime = "6:30 AM";
+let sunsetTime = "6:00 PM";
+
+console.log(temperature);
+console.log(conditions);
+console.log(sunriseTime);
+console.log(sunsetTime);
+
+console.log(`It's currently ${conditions} and ${temperature} degrees. 
+            The sun rose this morning at ${sunriseTime} and will set at ${sunriseTime}.`);
