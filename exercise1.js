9a0d108f91bec50e3c8cb675a51c41179d32aa6b
ltt24 <dovohuongthao4902@gmail.com>
2026-04-06 11:29:34 +0700

    updated exercise 1

--- exercise1.js
+++ exercise1.js
@@ -1,4 +1,4 @@
-let temperature = 75;
+let temperature = 32;
 let conditions = "sunny";
 let sunriseTime = "6:30 AM";
 let sunsetTime = "6:00 PM";
@@ -8,5 +8,6 @@ console.log(conditions);
 console.log(sunriseTime);
 console.log(sunsetTime);
 
-console.log(`It's currently ${conditions} and ${temperature} degrees. 
-            The sun rose this morning at ${sunriseTime} and will set at ${sunriseTime}.`);
+console.log(`It's currently ${conditions} and ${temperature} degrees Celsius. 
+            The sun rose this morning at ${sunriseTime}  “today’s weather
+            and will set at ${sunriseTime}.`);
