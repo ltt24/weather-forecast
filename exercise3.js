d1f9a0f3eeadd44d1aaad197c15d68116d23eac6
ltt24 <dovohuongthao4902@gmail.com>
2026-04-06 12:22:03 +0700

    Weather Object

---
+++ exercise3.js
@@ -0,0 +1,7 @@
+let weather = {
+    temperature: 32,
+    conditions: "sunny"
+}
+
+console.log(weather.temperature);
+console.log(`It's ${weather.temperature} degrees Celsius and ${weather.conditions}.`);
